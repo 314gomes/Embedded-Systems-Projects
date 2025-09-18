d6e85a27934d8fdbeb50c496538ba192979b0ce5
Giu Olivato <134548196+Yolo-BR@users.noreply.github.com>
2025-09-17 23:28:10 -0300

    Update pwm.py

--- Experiments/Experiment 2/pwm.py
+++ Experiments/Experiment 2/pwm.py
@@ -1,16 +1,23 @@
+# Importa as bibliotecas necessárias para o código funcionar
 import RPi.GPIO as GPIO
 from time import sleep
 
+#Aqui criamos uma variável para facilmente alterar o pino da GPIO caso fosse necessário.
 led_pin = 18
 
+#A função setmode define se vamos usar o padrão de numeração dos pinos da broadcom ou da placa em si. Abaixo selecionamos o padrão da broadcom
 GPIO.setmode(GPIO.BCM)
 
+#definimos o pino escolhido como saída.
 GPIO.setup(led_pin, GPIO.OUT)
 
+#Na funçaõ PWM abaixo definimos o Pino do LED e a frequência que o PWM irá acionar
 pwm_led = GPIO.PWM(led_pin , 100)
 
+#Abaixo ligamos o LED
 pwm_led.start(0)
 
+#No loop abaixo iteramos o ciclo de trabalho de forma que gradativamente o LED fique mais aceso, o sleep garante uma transição suave para o processo.
 for i in range(100):
 	pwm_led.ChangeDutyCycle(i)
 	sleep(0.1)
