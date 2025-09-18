0809083c123a0a035d4a4d9caa05801d6271796d
Giu Olivato <134548196+Yolo-BR@users.noreply.github.com>
2025-09-17 23:33:07 -0300

    Update PIR_sensor.py

--- Experiments/Experiment 2/PIR_sensor.py
+++ Experiments/Experiment 2/PIR_sensor.py
@@ -1,18 +1,24 @@
+# Importa apenas as bibliotecas necessárias para o código funcionar
 from gpiozero import MotionSensor,LED
 from signal import pause
+
+#definimos variáveis para alterar os pinos do LED e do PIR, caso fosse necessário.
 pir = MotionSensor(18)
 led = LED(15)
 
+#se detectar movimento: Acende o LED
 def motion_function():
     print("Motion Detected")
     led.on()
 
+#se não detectar movimento: apaga o LED, ou, se for a primeira vez, não acende o LED.
 def no_motion_function():
     print("Motion stopped")
     led.off()
 
-
+#Usa a função when_motion/when_no_motion para definir o que o código deve executar caso haja movimento ou caso não haja movimento.
 pir.when_motion = motion_function
 pir.when_no_motion = no_motion_function
 
+#permite que o código não seja encerrado depois da primeira vez iniciado, isto é: ele sempre aguarda que um dos dois estados aconteça para poder começar a funcionar.
 pause()
