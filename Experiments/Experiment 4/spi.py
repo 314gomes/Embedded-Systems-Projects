099b8f0280897c3994133c430883cff072f8ba82
Giu Olivato <134548196+Yolo-BR@users.noreply.github.com>
2025-10-15 15:28:49 -0300

    Upload do spi.py
    Adicionei o código spi.py que utilizamos na prática.

---
+++ Experiments/Experiment 4/spi.py
@@ -0,0 +1,31 @@
+# Para descobrir qual a codificação da Tag com texto gravado anteriormente
+from mfrc522 import SimpleMFRC522
+from time import sleep
+import RPi.GPIO as GPIO
+#desabilitar os avisos
+GPIO.setwarnings(False)
+#cria o objeto "leitor" para a instância "SimpleMFRC522" da biblioteca
+leitor = SimpleMFRC522()
+
+#GPIO.setmode(GPIO.BCM)
+
+red_led_pin = 3
+green_led_pin = 5
+
+GPIO.setup(red_led_pin, GPIO.OUT)
+GPIO.setup(green_led_pin, GPIO.OUT)
+
+
+print("Aproxime a tag do leitor para leitura.")
+while True: #loop
+#cria as variáveis "id" e "texto", e as atribui as leituras da id e do texto coletado da tag pelo leitor, respectivamente
+	id,texto = leitor.read()
+	print("ID: {}\nTexto: {}".format(id, texto)) #exibe as informações coletadas
+	if id == 712936067078:
+		GPIO.output(green_led_pin, GPIO.HIGH)
+		GPIO.output(red_led_pin, GPIO.LOW)
+	else:
+                GPIO.output(green_led_pin, GPIO.LOW)
+                GPIO.output(red_led_pin, GPIO.HIGH)
+
+	sleep(3) #aguarda 3 segundos para nova leitura
